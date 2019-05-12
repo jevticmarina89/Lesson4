8851af432acd235078b19bd0672c2dc0be193d90
jevticmarina89 <marina.jevtic89@gmail.com>
2019-05-13 01:54:03 +0200

    method to draw a shell

--- src/exercises/TriangleShell.java
+++ src/exercises/TriangleShell.java
@@ -1,31 +1,32 @@
 package exercises;
 
-public class TriangleShell {
+import org.jointheleague.graphical.robot.Robot;
 
-	// 1. Instantiate a new Robot
+public class TriangleShell {
+	
+	Robot robot = new Robot();
 
 	void go() {
-		drawTriangle(100);	//3. Remove this command, it's just for testing
 
-		// 6. Make the robot go as fast as possible
+		robot.setSpeed(100);
 
-		// 4. make a variable to hold the length of the triangle and set it to 50
-
-		// 7. Do all the steps below, 60 times
-
-			// 9. Change the color of the pen to a random color
 	
-			// 8. Increase the length of the side by 10 pixels
-	
-			// 5. call your drawTriangle() method using your length variable
-
-			// 10. Turn the robot 6 degrees to the right
-
-	}
-
-	/* 2. Fill in the method below to draw a triangle. Use the length variable for the size of the triangle. */
-	private void drawTriangle(int length) {
+		int length = 50;
 		
+		for (int i = 0; i < 60; i++) {
+			robot.setRandomPenColor();
+			length += 10;
+			drawTriangle(length);
+			robot.turn(6);
+		}
+}
+
+	private void drawTriangle(int length) {
+		robot.penDown();
+		for (int i = 0; i < 3; i++) {
+			robot.move(length);
+			robot.turn(120);
+		}
 		
 	}
 
