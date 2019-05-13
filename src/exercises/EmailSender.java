b3fac9ddb08096c20ccac8be01125494f3848ab6
jevticmarina89 <marina.jevtic89@gmail.com>
2019-05-13 02:02:26 +0200

    method for verifying email

--- src/exercises/EmailSender.java
+++ src/exercises/EmailSender.java
@@ -2,6 +2,8 @@ package exercises;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class EmailSender {
 
 	void go() {
@@ -9,13 +11,32 @@ public class EmailSender {
 		 * Use the methods below to send an email and print a warning if the email did
 		 * not send correctly.
 		 */
-
+		String email = JOptionPane.showInputDialog("Enter new email address:");
+		setEmailAddress(email);
 		
+		String text = JOptionPane.showInputDialog("Enter your text:");
+		
+		if (send(text) && correctAdress(email))
+			JOptionPane.showMessageDialog(null, "Email sent");
+		else
+			JOptionPane.showMessageDialog(null, "The email did not send correctly");
+
 	}
+
+
+
+	boolean correctAdress(String email) {
+		if (!(this.emailAddress.contains("@") && this.emailAddress.contains("."))) {
+			System.err.println("Error, not valid email address.");
+			return false;
+		}
+		return true;
+	}
+		
 
 	/* Returns true if email was sent successfully, false if it failed to send. */
 	boolean send(String greeting) {
-		if (this.emailAddress.isBlank()) {
+		if (this.emailAddress.isEmpty()) {
 			System.err.println("You did not provide a recipient for the email!");
 			return false;
 		}
